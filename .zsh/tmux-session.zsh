d1b3106d17e328020322b6f995635ddeed937216
Paweł Słowik <pawel.slowik@zoho.com>
2018-12-27 12:40:00 +0100

    Start the tmux server, if not already running

--- .zsh/tmux-session.zsh
+++ .zsh/tmux-session.zsh
@@ -15,7 +15,8 @@ function tmux_session() {
 			echo "no such session: $session_name" 1>&2
 			return 1
 		fi
-		commands=(source-file "$session_file")
+		commands=(start-server)
+		commands+=( \; source-file "$session_file")
 	fi
 	tmux "${commands[@]}"
 }
