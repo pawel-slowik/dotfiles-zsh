801281118fa3213285dd3c2e78474d506982ffad
Paweł Słowik <pawel.slowik@zoho.com>
2024-09-22 12:07:14 +0200

    Add support for the clipboard X selection

--- .zsh/x-clipboard.zsh
+++ .zsh/x-clipboard.zsh
@@ -4,12 +4,22 @@ fi
 
 # https://unix.stackexchange.com/a/219678
 
-function paste-xclip() {
+function paste-xclip-primary() {
 	emulate -L zsh
 	killring=("$CUTBUFFER" "${(@)killring[1,-2]}")
 	CUTBUFFER=$(xclip -selection primary -o)
 	zle yank
 }
 
-zle -N paste-xclip
-bindkey "^xp" paste-xclip
+function paste-xclip-clipboard() {
+	emulate -L zsh
+	killring=("$CUTBUFFER" "${(@)killring[1,-2]}")
+	CUTBUFFER=$(xclip -selection clipboard -o)
+	zle yank
+}
+
+zle -N paste-xclip-primary
+bindkey "^xp" paste-xclip-primary
+
+zle -N paste-xclip-clipboard
+bindkey "^xx" paste-xclip-clipboard
