dd4aa2100bf1478a99ac6c7883452189437f722d
Paweł Słowik <pawel.slowik@zoho.com>
2024-10-16 21:59:12 +0200

    Remove unused aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -2,6 +2,7 @@ alias cp='/bin/cp -i'
 alias mv='/bin/mv -i'
 alias rm='/bin/rm -i'
 alias ln='/bin/ln -i'
+
 alias less='/usr/bin/less -M'
 alias v='ls -hoB --time-style=long-iso'
 alias now='/bin/date +"%Y-%m-%d %H:%M:%S %z"'
@@ -9,11 +10,7 @@ alias grep='grep --color=auto'
 alias rs='/usr/bin/rsync -r --progress --stats -h'
 alias sqlite3='/usr/bin/sqlite3 -header'
 alias jqr="jq -r -R '. as \$line | try fromjson catch \$line'"
+alias highlight-html='highlight --syntax html --out-format xterm256'
 
-alias term='xterm &!'
-
-alias mpg123='/usr/bin/mpg123 --index --gapless --smooth --control --title --verbose --output alsa'
-alias mplayer-hdmi='mplayer -ao alsa:device=hdmi'
 alias ffprobe='ffprobe -hide_banner'
 alias ffmpeg='ffmpeg -hide_banner'
-alias highlight-html='highlight --syntax html --out-format xterm256'
